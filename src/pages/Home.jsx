72bb5507bad9426bc780b766c2ce4b8eeddcb041
engrusmanbelloa <usmanbelloa@gmail.com>
2022-10-20 09:37:37 -0400

    homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+import Categories from '../components/Category'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
+import Products from '../components/Products'
+import Slider from '../components/Slider'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Announcement/>
+        <Navbar/>
+        <Slider/>
+        <Categories/>
+        <Products/>
+        <Newsletter/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
